5dd88a28aa0a134b7fa5c23cab5c5fe92f554f74
Archer03 <soulking1996@outlook.com>
2022-03-29 23:59:23 +0800

    feature: only keep tracked for draft type

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,8 +7,25 @@ export interface Conveyor {
   autorun: () => void
 }
 
-type UseConveyor<T> = <K>(selector?: (operators: any) => K) => K extends unknown
+type UseConveyor<T> = <K>(selector?: (operators: any) => K) => K extends undefined
   ? [selected: T, modifyFn: ModifyFunction<T>]
-  : [selected: K, modifyFn: ModifyFunction<K>]
+  : [selected: K, modifyFn: ModifyFunction<Draft<K>>]
 
 type ModifyFunction<T> = (producer: (draft: T) => void) => void
+
+/**
+ * usage:
+ * ({ track }) => ({ dogAge: track('dog', 'age') as TrackedValue<number> })
+ */
+export type TrackedValue<T> = { TRACKED: T }
+
+/**
+ * get key list which for tracked props
+ */
+type TrackKeys<T> = {
+  [K in keyof T]: T[K] extends TrackedValue<unknown> ? K : never
+}[keyof T]
+
+type Draft<T> = {
+  [K in TrackKeys<T>]: T[K] extends TrackedValue<unknown> ? T[K]['TRACKED'] : never
+}
